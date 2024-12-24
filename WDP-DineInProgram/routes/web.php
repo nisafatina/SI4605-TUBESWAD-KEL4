598ff35e2971fda537024e167573d5d990890498
nisafatina <nisafatina298@gmail.com>
2024-12-24 14:05:23 +0700

    Dashboard

--- WDP-DineInProgram/routes/web.php
+++ WDP-DineInProgram/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route tunggal dengan logika kondisi
 Route::get('/', function () {
-    return view('welcome');
+    $showStarterPage = true; // Ganti dengan kondisi yang sesuai
+    if ($showStarterPage) {
+        return view('layouts.index');
+    }
+    return view('layouts.starter-page');
 });
