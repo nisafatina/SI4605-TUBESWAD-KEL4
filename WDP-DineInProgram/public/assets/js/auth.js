c94665e6255d300d55aef92aed4815820cb01d71
nisafatina <148049968+nisafatina@users.noreply.github.com>
2024-12-30 17:19:49 +0700

    Delete WDP-DineInProgram directory

--- WDP-DineInProgram/public/assets/js/auth.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    setTimeout(function() {
-        const errorMessages = document.querySelectorAll('.error');
-        errorMessages.forEach(function(error) {
-            error.style.display = 'none';
-        });
-    }, 3000); // 3 detik setelah halaman dimuat
-}
